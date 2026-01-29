ad5ab2e71a09ab652cc11e5ddbe3b50f638cc994
vxdqqqq <streamking@ukr.net>
2026-01-29 20:34:56 +0200

    changed index.js

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the Node.js app deployed via AWS CI/CD pipeline!');
+  res.send('Hello from the Node.js app deployed via AWS CI/CD pipeline! Hello from Ukraine!');
 });
 
 app.listen(port, () => {
